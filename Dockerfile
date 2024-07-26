dc2f01dc9f82cdbcf56a2c29de69adf5a3d85162
KasambaLumwagi <kasambalumwagi@gmail.com>
2024-07-26 10:01:33 +0300

    modified:   Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,12 @@ FROM apache/airflow:2.7.1-python3.11
 
 USER root
 RUN apt-get update && \
-    apt-get install -y gcc python3-dev openjdk-11-jdk && \
-    apt-get clean\
+    apt-get install -y gcc python3-dev openjdk-17-jdk && \
+    apt-get clean && \
     apt-get update && apt-get install -y procps 
 # Set JAVA_HOME environment variable
-ENV JAVA_HOME /usr/lib/jvm/java-11-openjdk-arm64
-RUN  export JAVA_HOME 
+ENV JAVA_HOME /usr/lib/jvm/java-17-openjdk-amd64
 USER airflow
 
 RUN pip install apache-airflow apache-airflow-providers-apache-spark pyspark
+    
--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -4,31 +4,34 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 dag = DAG(
-    dag_id = "sparking_flow",
-    default_args = {
+    dag_id="Flow",
+    default_args={
         "owner": "Kasamba Lumwagi",
         "start_date": airflow.utils.dates.days_ago(1)
     },
-    schedule = "@daily"
+    schedule_interval="@daily"
 )
 
 start = PythonOperator(
     task_id="start",
-    python_callable = lambda: print("Jobs started"),
+    python_callable=lambda: print("Jobs started"),
     dag=dag
 )
 
 python_job = SparkSubmitOperator(
     task_id="python_job",
     conn_id="spark-conn",
-    application="jobs/python/wordcountjob.py",
+    application="/opt/bitnami/spark/jobs/python/wordcountjob.py",
+    name="arrow-spark",
+    conf={
+        "spark.master": "spark://spark-master:7077",
+    },
     dag=dag
 )
 
-
 end = PythonOperator(
     task_id="end",
-    python_callable = lambda: print("Jobs completed successfully"),
+    python_callable=lambda: print("Jobs completed successfully"),
     dag=dag
 )
 
