5246d41bdea3ba2e0567fbae9aaf66685c0b4f4d
KasambaLumwagi <kasambalumwagi@gmail.com>
2024-08-07 14:17:45 +0300

    new file:   WordCountJob.java 	new file:   ../../../../../test/java/com/xamba/spark/AppTest.java

---
+++ jobs/java/spark-job/src/main/java/com/xamba/spark/WordCountJob.java
@@ -0,0 +1,13 @@
+package com.xamba.spark;
+
+/**
+ * Hello world!
+ *
+ */
+public class WordCountJob
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World my name is Kasamba!" );
+    }
+}
